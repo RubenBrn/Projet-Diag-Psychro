4f827e42d5b4f174bfc33fa29163d4fb6078f70c
aliceboudet <a.boudet96@gmail.com>
2017-03-17 11:58:26 +0100

    main_séance 3

--- main_fonctionnel.m
+++ main_fonctionnel.m
@@ -16,17 +16,17 @@ Mas= 28.965338*10^-3 ; %kg/mol
 Mv= 18.01528*10^-3 ; 
 
 % Variables
-Tmin=5;
+Tmin=-15;
 Tmax=50;
 
-T=linspace(Tmin, Tmax, 10000);
+T=linspace(Tmin, Tmax, 100);
 
 %Nb de courbes iso
 
 
 nb_iso=20; %nombre de courbes iso par fenetre
 
-title('Abaque psychrometrique simplifie')
+
 
 
 
@@ -47,23 +47,61 @@ h_max=300;
 
 
 
-axes=gca;
-axes.YAxisLocation='right';
+%% Tracé du diagramme psychrométrique
 
-%creation d'un nouvel axe
+figure()
+hold all
 
 
-figure(1)
+%%%%%%%%%%%%%%%%%%%%%%%% Mise en forme  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-hold 
+% 'stem' trace les traits verticaux sous la courbe de saturation
+
+title('Abaque psychrometrique simplifie')
+
+xlabel('T en °C')
+ylabel('omega en kg/kg')
+
+xlim([Tmin Tmax])
 ylim([0 0.05])
+
+% Gestion des axes
+axes=gca;
+axes.YAxisLocation='right'
+axes.FontSize=12;
+%axes.FontWeight='bold';
+
+%Echelles secondaires
+
+Pvs=pression_vapPa(T);
+omega=0.622.*(Pvs./(Ptot-Pvs));
+
+pressions_partiellesPa=pressionpartielle_fomega(omega,Ptot);
+pressions_partielleskgcm2=pressions_partiellesPa.*1E-5;
+
+plotyy(T,omega,T,pressions_partielleskgcm2,'plot');
+
+%% Tracé de la courbe de saturation
+
 Pvs=pression_vapPa(T);
 courbe_sat=0.622.*(Pvs./(Ptot-Pvs));
 
-for i=1:10
-    plot(T, 0.1*i*courbe_sat, '-k')
+Tlegende=2*Tmax/3;
+Pvs_legende=pression_vapPa(Tlegende);
+courbe_sat_legende=0.622.*(Pvs_legende./(Ptot-Pvs_legende));
+
+pas=0.2;
+for i=pas:pas:1
+    plot(T, i*courbe_sat, '-k')
+    n=i*100;
+    str=['\epsilon=' num2str(n) '%'];
+    xt=Tlegende;
+    yt=(i*courbe_sat_legende)+(0.0007)*exp(i/2); 
+    text(xt,yt,str,'FontSize',10,'Color','black','rotation',60*i)
 end 
 
+%'Extent',[xt-1 yt-0.0015 3 0.003] pour tracer un carré blanc autour du
+%texte mais ça marche pas
 
 %% courbes isenthalpes 
 
