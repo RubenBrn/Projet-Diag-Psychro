b763c2d908950d397f6e0720e849732c956d62dc
RubenBrn <ruben.brunetaud@gmail.com>
2017-04-15 18:37:32 +0200

    Revert "Merge branch 'ratio' into Master"
    This reverts commit 583b898b537ee38ff2a84c15979660948deda03f, reversing
    changes made to c127183fe4a026fdf8e2fbab25e87dc5c167df51.

--- affichage_logo.m
+++ affichage_logo.m
@@ -3,32 +3,19 @@ clear all
 close all
 
 [scene,palette1]=imread('Diagramme.png');
-if ~isempty(palette1)
-    Im = ind2rgb(scene,palette1);
-end
-[obj,palette2]=imread('logo.jpg');
-if ~isempty(palette2)
-    Im = ind2rgb(obj,palette2);
-end
-obj=imresize(obj,0.5);
 
+[obj,palette2]=imread('logo.jpg');
+obj=imresize(obj,4);
 [largeur, longueur]=size(obj)
 
 
 % Now replace pixels in the scene with the object image
 result = scene;
 %   Define where you want to place the object image in the scene
-rowshift = 0; % d??calage depuis le haut
-colshift = 0; % d??calage depuis la gauche
+rowshift = 200; % d??calage depuis le haut
+colshift = 300; % d??calage depuis la gauche
 %   Perform the actual indexing to replace the scene's pixels with the object
-imax=largeur;
-jmax=longueur;
-for i=1:imax
-    for j=1:jmax
-        
-        result(i,j) = obj(i,j);
-    end 
-end 
+result((1:largeur)+rowshift, (1:longueur)+colshift) = obj;
 %   Display the results
 
 
